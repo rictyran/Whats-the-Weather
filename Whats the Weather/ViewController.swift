b88e6378bc4972ee11c6f796403396ec0e3c3893
rictyran <rictyran@gmail.com>
2015-04-16 14:55:20 -0400

    Swift weather app tutorial - practice using data retrieved with NSURL
    final

---
+++ Whats the Weather/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Whats the Weather
+//
+//  Created by Richard Tyran on 4/16/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var userCity: UITextField!
+    
+    
+    @IBAction func findWeather(sender: AnyObject) {
+    
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                var urlError = false
+                
+                var weather = ""
+                
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    println(urlContent)
+                    
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray.count > 0 {
+                        
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span")
+                        
+                        weather = weatherArray[0] as String
+                        
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                    } else {
+                        
+                        urlError = true
+                        
+                    }
+                    
+                    println(urlContentArray[1])
+                    
+                    
+                } else {
+                    
+                    urlError = true
+                    
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    if urlError == true {
+                        
+                        self.showError()
+                        
+                    } else {
+                        
+                        self.resultLabel.text = weather
+                    }
+                }
+            })
+            
+            task.resume()
+            
+        } else {
+            
+            showError()
+            
+        }
+
+    }
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    func showError() {
+        
+        resultLabel.text = "Was not able to find weather for " + userCity.text + ". Please try again"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+            }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
